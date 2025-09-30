69fc2fcaadbcdd45a75656ad0ae9f2db61656bdf
cgrevy <chgre23@student.sdu.dk>
2025-09-30 10:33:37 +0200

    save

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CarRentalCard from './CarRentalCard'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CarRentalCard image={require('./assets/red-sports-car-png-1.png')}
+       rental_price={120}model='A8' brand='Audi'/>
+      <CarRentalCard image={require('./assets/Peugeot_206_front_20090416.jpg')}
+       rental_price={200}
+       brand='Peugeot' 
+       model='206+'/>
     </View>
+
   );
 }
 
