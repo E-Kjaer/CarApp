a41428586e1e997dbc705e0531deb133fd2f3d17
Magnus Hende Christensen <magnushende@gmail.com>
2025-09-30 10:51:45 +0200

    Added styles back in

--- App.tsx
+++ App.tsx
@@ -15,4 +15,13 @@ export default function App() {
     </View>
 
   );
+  
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
