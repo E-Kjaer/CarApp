8531b197675f9bb3e565cec8a43b8f9e3ea3fe61
Andreas528 <andreas.runge528@gmail.com>
2025-10-02 09:51:18 +0200

    finished creating booking page

--- Components/BookingPage.tsx
+++ Components/BookingPage.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
 
-const INPUT_WIDTH = 260; // tweak as you like
+const INPUT_WIDTH = 260; 
 
 export default function BookingPage() {
   return (
     <View style={styles.screen}>
+      <View style={styles.header}>
+        <Text style={styles.headerTxt}>How long would you like to book the vehicle?</Text>
       <View style={styles.content}>
         <Text style={styles.label}>Start Date</Text>
-        <TextInput placeholder="YYYY-MM-DD" style={styles.input} />
+        <TextInput placeholder="DD-MM-YYYY" style={styles.input} />
 
         <Text style={styles.label}>End Date</Text>
-        <TextInput placeholder="YYYY-MM-DD" style={styles.input} />
+        <TextInput placeholder="DD-MM-YYYY" style={styles.input} />
 
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Confirm Dates</Text>
         </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -24,12 +27,20 @@ export default function BookingPage() {
 const styles = StyleSheet.create({
   screen: { flex: 1, backgroundColor: "#fff" },
   header: {
-    
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 225,
+  },
+  headerTxt:{
+    fontSize: 15,
+    fontWeight: "bold",
   },
   content: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",   // centers the block horizontally
+    alignItems: "center",   
     gap: 12,
     paddingHorizontal: 16,
   },
@@ -38,7 +49,7 @@ const styles = StyleSheet.create({
     textAlign: "left",       
     fontSize: 14,
     fontWeight: "600",
-    marginBottom: 6,
+    marginBottom: 1,
   },
   input: {
     width: INPUT_WIDTH,      
@@ -54,8 +65,8 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 16,
     borderRadius: 999,
-    backgroundColor: "#1e90ff",
-    marginTop: 8,
+    backgroundColor: "#6351a9",
+    marginBottom: 325,
   },
   buttonText: { color: "#fff", fontWeight: "700", fontSize: 15 },
 });
