a8f102775ec086a94b81c5f5ca0f3fa2b036ec4a
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-10-09 12:10:28 +0200

    Added alerts on failed login/signup
    Alerts work only on mobile and require the server to send an error, no form validation yet....

--- Views/SignupPage.tsx
+++ Views/SignupPage.tsx
@@ -4,6 +4,7 @@ import {
     TextInput,
     TouchableOpacity,
     Text,
+    Alert
 } from "react-native";
 import { useAuth } from "../Contexts/Authcontext";
 import { useState } from "react";
@@ -46,6 +47,7 @@ export default function SignupScreen({ navigation }: any) {
             })
             .catch((error) => {
                 console.log(error);
+                Alert.alert("Signup failed", "",[{text: "OK"}])
             });
     };
 
