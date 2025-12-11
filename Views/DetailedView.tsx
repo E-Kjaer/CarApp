b5c1166f0b23d46460c9a96f9f1f28ac504d1562
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-12-11 10:42:39 +0100

    Hotfix for Details View

--- Views/DetailedView.tsx
+++ Views/DetailedView.tsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ActivityIndicator, FlatList, Image, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Pressable,
+} from "react-native";
 import api from "../api";
-import {RouteProp, useNavigation, useRoute} from "@react-navigation/native";
-import {ExploreStackParamList} from "../Navigation/ExploreNavTypes";
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {Ionicons} from "@expo/vector-icons";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { ExploreStackParamList } from "../Navigation/ExploreNavTypes";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
 
 type DetailedRoute = RouteProp<ExploreStackParamList, "Detailed">;
 type DetailedNav = NativeStackNavigationProp<ExploreStackParamList, "Detailed">;
-
 
 interface Car {
   car_id: number;
@@ -29,16 +35,16 @@ interface Car {
 }
 
 interface Owner {
-  user_id: number,
-  name: string,
-  email: string,
-  phonenumber: number,
-  rating: number
+  user_id: number;
+  name: string;
+  email: string;
+  phonenumber: number;
+  rating: number;
 }
 
 export default function DetailedView() {
   const insets = useSafeAreaInsets();
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   const { params } = useRoute<DetailedRoute>();
   const [car, setCar] = useState<Car>();
   const [owner, setOwner] = useState<Owner>();
@@ -46,142 +52,186 @@ export default function DetailedView() {
 
   useEffect(() => {
     api
-        .get<Car>("/getCar/" + params.car_id)
-        .then((res) => setCar(res.data))
-        .catch((err) => console.warn("API error:", err))
-        .finally(() => setLoading(false));
+      .get<Car>("/cars/" + params.car_id)
+      .then((res) => setCar(res.data))
+      .catch((err) => console.warn("API error:", err))
+      .finally(() => setLoading(false));
   }, [params.car_id]);
 
   useEffect(() => {
-    if(!car?.owner_id) return;
+    if (!car?.owner_id) return;
     api
-      .get<Owner>("/getUser/" + car?.owner_id)
+      .get<Owner>("/users/" + car?.owner_id)
       .then((res) => setOwner(res.data))
-      .catch((err) => console.warn("API error", err))
+      .catch((err) => console.warn("API error", err));
   }, [car?.owner_id]);
 
   console.log(owner?.user_id);
   if (loading) return <ActivityIndicator style={{ flex: 1 }} size="large" />;
   if (car && owner)
     return (
-    <View style={{
-      marginTop: insets.top,
-      flex: 1, 
-      padding: 0}}>
-      <Image source={{uri: `http://localhost:3000/${car.image}`}}
-        style={styles.image}
-      />
-      <View style={styles.brand_container}>
-        <Text style={styles.car_title}>{car.brand} {car.model}</Text>
-        <Text style={styles.sub_title}>{car.year}</Text>
-      </View>
-      <View style={styles.tags_container}>
-        <View style={styles.tag}>
-          <Ionicons name={"battery-charging-outline"} size={18}
-                    style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-          <Text style={styles.tag_text}>{car.fuel_type}</Text>
+      <View
+        style={{
+          marginTop: insets.top,
+          flex: 1,
+          padding: 0,
+        }}
+      >
+        <Image
+          source={{ uri: `http://localhost:3000/${car.image}` }}
+          style={styles.image}
+        />
+        <View style={styles.brand_container}>
+          <Text style={styles.car_title}>
+            {car.brand} {car.model}
+          </Text>
+          <Text style={styles.sub_title}>{car.year}</Text>
         </View>
-        <View style={styles.tag}>
-          <Ionicons name={"analytics-outline"} size={18}
-                    style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-          <Text style={styles.tag_text}>{car.range} km</Text>
-        </View>
-        <View style={styles.tag}>
-          <Ionicons name={"people-outline"} size={18}
-                    style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-          <Text style={styles.tag_text}>{car.seats} seats</Text>
-        </View>
-      </View>
-      <View style={styles.information_container}>
-        <Text style={styles.title}>Vehicle Information</Text>
-        <Text>{car.description}</Text>
-      </View>
-      <View style={styles.information_container}>
-        <View style={styles.header_owner}>
-          <View style={styles.info}>
-            <Ionicons name={"person"} size={32}
-                      style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-            <Text style={styles.title}>{owner.name}</Text>
+        <View style={styles.tags_container}>
+          <View style={styles.tag}>
+            <Ionicons
+              name={"battery-charging-outline"}
+              size={18}
+              style={{ marginRight: 6 }}
+              color="#6351a9"
+            ></Ionicons>
+            <Text style={styles.tag_text}>{car.fuel_type}</Text>
           </View>
-        <View style={styles.rating}>
-          <Ionicons name={"star"} size={32}
-                    style={{ marginRight: 6 }}  color="#6351a9">
-          </Ionicons>
-          <Text style={styles.title}>{owner.rating}/5</Text>
+          <View style={styles.tag}>
+            <Ionicons
+              name={"analytics-outline"}
+              size={18}
+              style={{ marginRight: 6 }}
+              color="#6351a9"
+            ></Ionicons>
+            <Text style={styles.tag_text}>{car.range} km</Text>
+          </View>
+          <View style={styles.tag}>
+            <Ionicons
+              name={"people-outline"}
+              size={18}
+              style={{ marginRight: 6 }}
+              color="#6351a9"
+            ></Ionicons>
+            <Text style={styles.tag_text}>{car.seats} seats</Text>
+          </View>
         </View>
+        <View style={styles.information_container}>
+          <Text style={styles.title}>Vehicle Information</Text>
+          <Text>{car.description}</Text>
         </View>
+        <View style={styles.information_container}>
+          <View style={styles.header_owner}>
+            <View style={styles.info}>
+              <Ionicons
+                name={"person"}
+                size={32}
+                style={{ marginRight: 6 }}
+                color="#6351a9"
+              ></Ionicons>
+              <Text style={styles.title}>{owner.name}</Text>
+            </View>
+            <View style={styles.rating}>
+              <Ionicons
+                name={"star"}
+                size={32}
+                style={{ marginRight: 6 }}
+                color="#6351a9"
+              ></Ionicons>
+              <Text style={styles.title}>{owner.rating}/5</Text>
+            </View>
+          </View>
 
-        <View style={styles.info}>
-          <Ionicons name={"mail"} size={18}
-                    style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-          <Text>{owner.email}</Text>
+          <View style={styles.info}>
+            <Ionicons
+              name={"mail"}
+              size={18}
+              style={{ marginRight: 6 }}
+              color="#6351a9"
+            ></Ionicons>
+            <Text>{owner.email}</Text>
+          </View>
+          <View style={styles.info}>
+            <Ionicons
+              name={"call"}
+              size={18}
+              style={{ marginRight: 6 }}
+              color="#6351a9"
+            ></Ionicons>
+            <Text>{owner.phonenumber}</Text>
+          </View>
         </View>
-        <View style={styles.info}>
-          <Ionicons name={"call"} size={18}
-                    style={{ marginRight: 6 }} color="#6351a9"></Ionicons>
-          <Text>{owner.phonenumber}</Text>
+        <View style={styles.buttom_bar}>
+          <Pressable
+            style={styles.rent_now_button}
+            onPress={() => {
+              navigation.navigate("Booking");
+            }}
+          >
+            <Ionicons
+              name={"calendar-outline"}
+              size={28}
+              color={"white"}
+            ></Ionicons>
+            <Text style={styles.rent_now_button_text}>
+              {" "}
+              {car.price} DKK/day
+            </Text>
+          </Pressable>
         </View>
       </View>
-      <View style={styles.buttom_bar}>
-        <Pressable style={styles.rent_now_button} onPress={() => {
-          navigation.navigate("Booking");
-        }}>
-          <Ionicons name={"calendar-outline"} size={28} color={"white"}></Ionicons>
-          <Text style={styles.rent_now_button_text}> {car.price} DKK/day</Text>
-        </Pressable>
-      </View>
-      </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  brand_container: { 
-    flex: 1 , 
-    flexDirection: "row", 
+  brand_container: {
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "space-between",
-    maxHeight: 45, 
-    padding: 0, 
+    maxHeight: 45,
+    padding: 0,
     marginTop: 10,
     marginLeft: 16,
-    marginRight: 16, 
+    marginRight: 16,
     borderWidth: 0,
-    borderRadius: 8, 
+    borderRadius: 8,
     //backgroundColor: "#FFFFFF"
   },
   information_container: {
-    flex: 1 , 
+    flex: 1,
     flexDirection: "column",
-    padding: 12, 
-    marginBottom: 10, 
+    padding: 12,
+    marginBottom: 10,
     marginLeft: 16,
-    marginRight: 16, 
+    marginRight: 16,
     borderRadius: 8,
-    backgroundColor: "#FFFFFF"
+    backgroundColor: "#FFFFFF",
   },
-  car_title: { 
-    fontSize: 25, 
-    fontWeight: "bold", 
+  car_title: {
+    fontSize: 25,
+    fontWeight: "bold",
     //color: "#6351a9"
   },
 
-  title: { 
-    fontSize: 18, 
-    fontWeight: "bold"},
-  sub_title: { fontSize:18 },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sub_title: { fontSize: 18 },
   price: {
     color: "#6351a9",
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   price_container: {
-    flex: 1, 
-    flexDirection: "row", 
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     maxHeight: 45,
     padding: 6,
-    maxWidth: 130, 
-    borderWidth: 1, 
-    borderRadius: 24, 
+    maxWidth: 130,
+    borderWidth: 1,
+    borderRadius: 24,
     borderColor: "#6351a9",
     backgroundColor: "#FFFFFF",
     marginRight: 6,
@@ -189,13 +239,13 @@ const styles = StyleSheet.create({
   rent_now_button: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 12, 
-    marginBottom: 0, 
+    padding: 12,
+    marginBottom: 0,
     height: 55,
     borderWidth: 0,
-    borderRadius: 8, 
+    borderRadius: 8,
     backgroundColor: "#6351a9",
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.08,
     shadowRadius: 12,
     shadowOffset: { width: 0, height: 6 },
@@ -218,18 +268,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   tags_container: {
-    flex: 1, 
-    flexDirection: "row", 
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "flex-start",
     padding: 0,
-    maxHeight: 38, 
+    maxHeight: 38,
     marginBottom: 10,
     marginLeft: 16,
-    marginRight: 16, 
+    marginRight: 16,
   },
   tag: {
-    flex: 1, 
-    flexDirection: "row", 
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     maxHeight: 45,
@@ -237,15 +287,14 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     borderRadius: 12,
     borderWidth: 1,
-    maxWidth: 100, 
+    maxWidth: 100,
     borderColor: "#6351a9",
     backgroundColor: "#FFFFFF",
-    marginRight: 6
+    marginRight: 6,
   },
   tag_text: {
     fontSize: 14,
     color: "#6351a9",
-  
   },
   header_owner: {
     flexDirection: "row",
@@ -254,20 +303,21 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderBottomWidth: 1,
     paddingBottom: 2,
-    borderColor: "#6351a9"
+    borderColor: "#6351a9",
   },
   info: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
   rating: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
-  image: { width: "100%", 
-    height: 200, 
-    marginTop: 8, 
+  image: {
+    width: "100%",
+    height: 200,
+    marginTop: 8,
     borderRadius: 8,
-    backgroundColor: "#FFFFFF"
-   },
+    backgroundColor: "#FFFFFF",
+  },
 });
--- Views/SignupPage.tsx
+++ Views/SignupPage.tsx
@@ -1,9 +1,9 @@
 import {
-    StyleSheet,
-    View,
-    TextInput,
-    TouchableOpacity,
-    Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
 } from "react-native";
 import { useAuth } from "../Contexts/Authcontext";
 import { useState } from "react";
@@ -12,127 +12,133 @@ import api from "../api";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SignupScreen({ navigation }: any) {
-    const insets = useSafeAreaInsets();
-    const { user, login, logout } = useAuth();
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [phonenumber, setPhonenumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [is_owner, setIs_Owner] = useState(false);
+  const insets = useSafeAreaInsets();
+  const { user, login, logout } = useAuth();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phonenumber, setPhonenumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [is_owner, setIs_Owner] = useState(false);
 
-    const signUp = (
-        password: string,
-        email: string,
-        name: string,
-        phonenumber: string,
-        is_owner: boolean
-    ) => {
-        api
-            .post("/insertUser", {
-                password: password,
-                email: email,
-                name: name,
-                phonenumber: phonenumber,
-                is_owner: is_owner ? 1 : 0,
-                rating: null
-            })
-            .then((res) => {
-                console.log('API Response:', res.data);
-                login(res.data);
-                navigation.navigate("Profile");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
+  const signUp = (
+    password: string,
+    email: string,
+    name: string,
+    phonenumber: string,
+    is_owner: boolean,
+  ) => {
+    api
+      .post("/users", {
+        password: password,
+        email: email,
+        name: name,
+        phonenumber: phonenumber,
+        is_owner: is_owner ? 1 : 0,
+        rating: null,
+      })
+      .then((res) => {
+        console.log("API Response:", res.data);
+        login(res.data);
+        navigation.navigate("Profile");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <View style={{justifyContent: "center", marginTop: insets.top}}>
-            <View style={styles.outerbox}>
-                <Text>Sign Up</Text>
-                <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={setEmail}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Password"
-                    value={password}
-                    secureTextEntry
-                    onChangeText={setPassword}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Phone number"
-                    value={phonenumber}
-                    onChangeText={setPhonenumber}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Full Name"
-                    value={name}
-                    onChangeText={setName}
-                    style={styles.input}
-                />
+  return (
+    <View style={{ justifyContent: "center", marginTop: insets.top }}>
+      <View style={styles.outerbox}>
+        <Text>Sign Up</Text>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={setPassword}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Phone number"
+          value={phonenumber}
+          onChangeText={setPhonenumber}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Full Name"
+          value={name}
+          onChangeText={setName}
+          style={styles.input}
+        />
 
-                <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 15 }}>
-                    <Checkbox
-                        value={is_owner}
-                        onValueChange={setIs_Owner}
-                        color={is_owner ? "#2196F3" : undefined}
-                        style={styles.checkbox}
-                    />
-                    <Text>Business Account?</Text>
-                </View>
-
-                <TouchableOpacity
-                    style={styles.loginButton}
-                    onPress={() => signUp(password, email, name, phonenumber, is_owner)}
-                >
-                    <Text>Sign Up</Text>
-                </TouchableOpacity>
-            </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginBottom: 15,
+          }}
+        >
+          <Checkbox
+            value={is_owner}
+            onValueChange={setIs_Owner}
+            color={is_owner ? "#2196F3" : undefined}
+            style={styles.checkbox}
+          />
+          <Text>Business Account?</Text>
         </View>
-    );
+
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => signUp(password, email, name, phonenumber, is_owner)}
+        >
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    outerbox: {
-        padding: 20,
-        margin: 20,
-        justifyContent: "center",
-    },
-    input: {
-        backgroundColor: "#fff",
-        borderWidth: 1,
-        borderColor: "#ccc",
-        borderRadius: 10,
-        paddingHorizontal: 15,
-        paddingVertical: 12,
-        fontSize: 16,
-        marginBottom: 15,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        elevation: 3,
-    },
-    loginButton: {
-        backgroundColor: "#ffffff",
-        borderWidth: 1,
-        borderColor: "#ccc",
-        borderRadius: 10,
-        padding: 15,
-        alignItems: "center",
-        marginBottom: 10,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        elevation: 3,
-    },
-    checkbox: {
-        marginRight: 8,
-    },
+  outerbox: {
+    padding: 20,
+    margin: 20,
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    fontSize: 16,
+    marginBottom: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  loginButton: {
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 15,
+    alignItems: "center",
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  checkbox: {
+    marginRight: 8,
+  },
 });
