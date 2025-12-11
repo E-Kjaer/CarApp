bbf0e99b7578247e72b7a4aa5d64096f728ea5d8
Andreas528 <andreas.runge528@gmail.com>
2025-12-11 12:24:59 +0100

    Created MyCarsView

--- Navigation/BottomNavBar.tsx
+++ Navigation/BottomNavBar.tsx
@@ -3,14 +3,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer} from "@react-navigation/native";
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
-import CarList from "../Views/CarList";
-import LoginPage from "../Views/LoginPage"
 import ExploreStack from "./ExploreStack";
 import ProfileStack from "./ProfileStack";
 import {useAuth} from "../Contexts/Authcontext";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import api from "../api";
-import MyCarsRentalCard from "../Components/MyCarsRentalCard";
+import MyCarsView from "../Views/MyCarsView";
 
 
 
@@ -21,54 +18,6 @@ export type RootTabParamList = {
 
 const Tab = createBottomTabNavigator();
 
-interface myCars {
-  owner_id: number,
-  car_id: number,
-  brand: string,
-  model: string,
-  image: string
-}
-
-function MyCarsScreen() {
-  const [myCars, setMyCars] = useState<myCars[]>([]);
-  const [loading, setLoading] = useState(true);
-  const {user, login, logout} = useAuth();
-  const [active, setActive] = useState<string[]>([]); 
-
-
-  const fetchMyCars = () => {
-    setLoading(true);
-    api.get<myCars[]>(`/users/${user?.user_id}/cars`, {
-      
-    })
-        .then((res) => setMyCars(res.data))
-        .catch((err) => console.warn("API error:", err))
-        .finally(() =>{
-            setLoading(false);
-        })
-  };
-
-  useEffect(() => {
-    fetchMyCars();
-  }, [active]);
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>My Cars</Text>
-      <FlatList
-      data={myCars}
-      keyExtractor={(item) => item.car_id.toString()}
-      renderItem={({ item }) => (
-          <MyCarsRentalCard
-              image={{uri: `http://localhost:3000/${item.image}`}}
-              model={item.model}
-              brand={item.brand}
-          />
-      )}
-      />
-    </View>
-  );
-}
   function BookingScreen() {
       return (
           <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -127,7 +76,7 @@ export default function BottomNavBar() {
             {user && (<Tab.Screen name="Bookings" component={BookingScreen} />
             )}
             {isOwner && (
-                <Tab.Screen name="MyCars" component={MyCarsScreen} />
+                <Tab.Screen name="MyCars" component={MyCarsView} />
             )}
           <Tab.Screen name = "Profile" component={ProfileStack} />
         </Tab.Navigator>
